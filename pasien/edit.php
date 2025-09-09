60bb6b1acca78596fa8f9e382a532d0299b49dd2
adha21 <adhamastalulala@gmail.com>
2025-09-09 18:13:14 +0700

    edit data

--- pasien/edit.php
+++ pasien/edit.php
@@ -1,3 +1,26 @@
+<?php
+######PROSES HAPUS#####
+
+#1. membuat koneksi
+include("../koneksi.php");
+
+#2. mengambil value ID edit
+$id = $_GET["id"];
+
+#3. menjalankan query edit
+$qry = mysqli_query($koneksi, "SELECT * FROM pasien WHERE pasienKlinik_ID = '$id'");
+
+#4. memisahkan field/kolom tabel pasien menjadi data array
+$row = mysqli_fetch_array($qry);
+
+$nama = $row['Nama_pasienKlinik'];
+$tgl_lahir = $row['Tanggal_LahirPasien'];
+$jk = $row['Jenis_kelaminPasien'];
+$alamat = $row['Alamat_Pasien'];
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,17 +43,17 @@
             <div class="col-7 m-auto mt-5">
                 <div class="card">
                     <div class="card-header">
-                        Form Data Pasien
+                        Form Edit Data Pasien
                     </div>
                     <div class="card-body">
                         <form method="post" action="proses_form.php">
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Nama Pasien</label>
-                                <input type="text" name="nama" placeholder="Masukkan Nama Lengkap" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <input type="text" name="nama" value="<?=$nama?>" placeholder="Masukkan Nama Lengkap" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Tanggal Lahir</label>
-                                <input type="date" name="tgl" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <input type="date" name="tgl" value="<?=$tgl_lahir?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Jenis Kelamin</label>
@@ -42,7 +65,7 @@
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Alamat</label>
-                                <input type="text" name="alamat" placeholder="Masukkan Alamat" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <input type="text" name="alamat" value="<?=$alamat?>" placeholder="Masukkan Alamat" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                             </div>
                             <button type="submit" class="btn btn-primary">Tambah</button>
                         </form>
--- pasien/index.php
+++ pasien/index.php
@@ -44,7 +44,7 @@
                                 $qry    = "SELECT * FROM pasien";
 
                                 #3. menjalankan query
-                                $result = mysqli_query($koneksi,$qry);
+                                $result = mysqli_query($koneksi, $qry);
 
                                 #4. melakukan looping data pasien
                                 $nomor  = 1;
@@ -60,11 +60,31 @@
                                         <td><?= $row['Jenis_kelaminPasien'] ?></td>
                                         <td><?= $row['Alamat_Pasien'] ?></td>
                                         <td>
-                                            <a class="btn btn-info btn-sm" href=""><i class="fa fa-pen-to-square"></i></a>
+                                            <a class="btn btn-info btn-sm" href="edit.php?id=<?= $row['pasienKlinik_ID'] ?>"><i class="fa fa-pen-to-square"></i></a>
 
-                                            <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#hapus<?= $data['id'] ?>">
+                                            <!-- Button trigger modal -->
+                                            <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $row['pasienKlinik_ID'] ?>">
                                                 <i class="fa-solid fa-trash"></i>
                                             </button>
+
+                                            <!-- Modal -->
+                                            <div class="modal fade" id="exampleModal<?= $row['pasienKlinik_ID'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Peringatan</h1>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            Yakin data pasien <?= $row['Nama_pasienKlinik'] ?> ingin dihapus?
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                            <a href="hapus.php?id=<?= $row['pasienKlinik_ID'] ?>" class="btn btn-danger">Hapus</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </td>
                                     </tr>
                                 <?php
